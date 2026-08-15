14c27ffbf8536514cc07c2069beb17c51f13c288
Paulo Gonçalves <Paulogrdc@gmail.com>
2026-08-15 11:40:47 -0300

    fiz a funcionalidade menu

--- __main__.py
+++ __main__.py
@@ -1,25 +1,9 @@
-from classes.cartas import Carta 
+from funcionalidades.funcoes import * 
 from rich import inspect 
 
 
 def main(): 
-    c1 = Carta(
-        estado = "Rj", 
-        codigo = "A1", 
-        nome_cidade = "Rio de janeiro", 
-        população= 1_000, 
-        pib=10_000, 
-        area=1200, 
-        pontos_turistico= 23)
-
-    c1.população = 3000
-    c1.pib = 20_000
-
-    print(f"A densidade Populacional é: {c1.densidade_populacional()}")
-    print(f"O Pib per capoita é: {c1.pib_per_capita():.2f}")
-    print(f"O Super Poder é: {c1.super_poder():.2f}")
-
-    inspect(c1,private=True, methods=True ) 
+    menu()
 
 
 
--- funcionalidades/funcoes.py
+++ funcionalidades/funcoes.py
@@ -1,8 +1,41 @@
+from classes.cartas import Carta 
+
 # --- função para guardar as mensagem de opação ---
-def menu():
+def guarda_mensagem():
+    print("---------------------------SEJA BEM-VINDO AO JOGO DE CARTAS---------------------------")
     print("1-Como funciona o jogo")
     print("2-Cadastrar cartas")
     print("3-Mostrar cartas cadastradas")
     print("4-Jogar")
     print("5-Fechar jogo")
+
+
+
+def menu(): 
+
+    while True: 
+        guarda_mensagem()
+        opcao = input("Escolha uma opação: ")
+
+        match opcao: 
+
+            case 1: 
+                print("Está funcionando!") 
+                
+            case 2: 
+
+                c1= Carta()
+                c2 = Carta()
+
+            case 3: 
+                pass 
+
+            case 4: 
+                pass 
+
+            case 5:
+                break
+ 
+
+
 
